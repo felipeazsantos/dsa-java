29d9ab3f416163477293361d2fa53a3c21809d2d
Felipe Azevedo <felipe.azevedo@cws.digital>
2025-02-15 13:30:14 -0300

    fix dynamic stack logic

--- src/main/java/dev/felipeazsantos/stack/DynamicStack.java
+++ src/main/java/dev/felipeazsantos/stack/DynamicStack.java
@@ -11,25 +11,19 @@ public class DynamicStack implements IStack {
     public void push(int element) {
         MyNode newNode = new MyNode();
         newNode.setElement(element);
-
-        if (isEmpty()) {
-            topNode = newNode;
-        } else {
-            MyNode auxNode = topNode;
-            topNode = newNode;
-            newNode.setNext(auxNode);
-        }
+        newNode.setNext(topNode);
+        topNode = newNode;
     }
 
     public int pop() {
         if (isEmpty()) {
             System.out.println("cannot pop, list is empty");
+            return -1;
         } else {
             int element = topNode.getElement();
             topNode = topNode.getNext();
             return element;
         }
-        return -1;
     }
 
     public boolean isEmpty() {
